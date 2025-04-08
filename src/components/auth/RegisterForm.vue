26965fdccf5c51e2782541e31592a11b93443c7c
arneldeloyola <arneldeloyola4@gmail.com>
2025-04-08 18:28:08 +0800

    fix: backgroud image and adjust the login and register view

--- src/components/auth/RegisterForm.vue
+++ src/components/auth/RegisterForm.vue
@@ -1,7 +1,7 @@
 <template>
   <v-form fast-fail @submit.prevent>
     <br />
-    <v-text-field label="Fullname" variant="outlined"></v-text-field>
+    <v-text-field label="Firstname" variant="outlined"></v-text-field>
     <v-text-field label="Lastname" variant="outlined"></v-text-field>
     <v-text-field label="Email" variant="outlined"></v-text-field>
 
