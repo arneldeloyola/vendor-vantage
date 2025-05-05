4fbd12e99b80b03c5a014f28900a3973b18d7634
Ralph <h2oralph1@gmail.com>
2025-05-05 08:42:52 +0800

    fix: Rework on login and register form

--- src/components/auth/LoginForm.vue
+++ src/components/auth/LoginForm.vue
@@ -2,7 +2,8 @@
 import { requiredValidator, emailValidator } from '@/utils/validators'
 import { ref } from 'vue'
 
-const isPasswordVisible = ref(false)
+const isPasswordVisible = ref(false) // This controls password visibility toggle
+
 const refVForm = ref()
 
 const formDataDefault = {
@@ -10,50 +11,49 @@ const formDataDefault = {
   password: '',
 }
 
-const formData = ref({
-  ...formDataDefault,
-})
+const formData = ref({ ...formDataDefault })
 
-const onLogin = () => {
-  //alert(formData.value.email)
+const onSubmit = () => {
+  // alert(formData.value.email)
 }
 
 const onFormSubmit = () => {
   refVForm.value?.validate().then(({ valid }) => {
-    if (valid) onLogin()
+    if (valid) onSubmit()
   })
 }
 </script>
 
 <template>
   <v-form ref="refVForm" @submit.prevent="onFormSubmit">
-    <br />
     <v-text-field
+      class="mb-3"
       v-model="formData.email"
-      prepend-inner-icon="mdi-email"
       label="Email"
+      prepend-inner-icon="mdi-email-outline"
+      variant="outlined"
       :rules="[requiredValidator, emailValidator]"
     ></v-text-field>
 
     <v-text-field
-      class="pt-2"
       v-model="formData.password"
-      prepend-inner-icon="mdi-lock"
+      prepend-inner-icon="mdi-lock-outline"
+      variant="outlined"
       label="Password"
       :type="isPasswordVisible ? 'text' : 'password'"
-      :append-inner-icon="isPasswordVisible ? 'mdi-eye' : 'mdi-eye-off'"
+      :append-inner-icon="isPasswordVisible ? 'mdi-eye-off' : 'mdi-eye'"
       @click:append-inner="isPasswordVisible = !isPasswordVisible"
       :rules="[requiredValidator]"
     ></v-text-field>
-
     <v-btn
-      class="mt-2 py-5"
+      class="mt-2"
+      elevation="12"
       type="submit"
       block
-      color="green-darken-3"
-      elevation="5"
+      color="grey-darken-1"
       prepend-icon="mdi-login"
       >Login</v-btn
     >
+    <v-divider class="my-5"></v-divider>
   </v-form>
 </template>
