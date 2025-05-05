07a3584712442c6709e298bb1d984318e376a6de
Ralph <h2oralph1@gmail.com>
2025-05-05 08:36:30 +0800

    feat: Added Homepage

---
+++ src/views/system/HomePage.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref } from 'vue'
+import TopNavbar from '../../components/layout/TopNavbar.vue'
+</script>
+
+<template>
+  <v-app>
+    <TopNavbar />
+
+    <v-main>
+      <v-container fluid class="pa-0">
+        <!-- Section 1 -->
+        <v-sheet color="grey-lighten-4" class="pa-6" width="100%"> </v-sheet>
+
+        <!-- Divider -->
+        <v-divider class="my-0" />
+
+        <!-- Section 2 -->
+        <v-sheet class="pa-6 pt-16" width="100%"> </v-sheet>
+
+        <!-- Divider -->
+        <v-divider class="my-0" />
+
+        <!-- Section 3 -->
+        <v-sheet color="grey-lighten-4" class="pa-6" width="100%"> </v-sheet>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
